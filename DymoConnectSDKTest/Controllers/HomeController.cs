70dba07abd1d5b4496b1821c2132365e24f152e6
ehs_yarles <ehs_yarles@iastate.edu>
2022-02-21 13:09:19 -0600

    comment

---
+++ DymoConnectSDKTest/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DymoSDK.Implementations;
+
+namespace DymoConnectSDKTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+
+        public ActionResult Print()
+        {
+            var label = DymoLabel.Instance;
+            var printers = DymoPrinter.Instance.GetPrinters();
+            var printer = printers.FirstOrDefault();
+            if (printer != null)
+            {
+                label.LoadLabelFromFilePath("generatedXML.dymo");
+
+                DymoPrinter.Instance.PrintLabel(label, printer.Name);
+            }
+            
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
